1894a1ca2e758c45c652521b4206c9b5c4436855
Leandro Abreu Ferreira <leandro.abreu@elaw.com.br>
2024-11-07 02:55:21 -0300

    feat: add type badge

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,24 @@
-const Transactions = () =>{
-  return <h1>transactions page</h1>;
+import { Button } from "@/app/_components/ui/button";
+import { DataTable } from "@/app/_components/ui/data-table";
+import { db } from "../_lib/prisma";
+import { ArrowDownUpIcon } from "lucide-react";
+import { transactionColumns } from "./_columns";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* TÍTULO E BOTÃO */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
 };
 
-export default Transactions;
+export default TransactionsPage;
